156446a9ac4973c623578967f90677dac5183095
t46 <takagi4646@gmail.com>
2022-07-14 07:41:07 +0900

    update inference.py

--- inference.py
+++ inference.py
@@ -1,3 +1,4 @@
+import json
 import torch
 import dataset
 import framework
@@ -33,13 +34,15 @@ def create_model_interface(model, label_smoothing):
     return model_interface
 
 
-def validate_on(set: torch.utils.data.Dataset, loader: torch.utils.data.DataLoader, model) -> Tuple[Any, float]:
+def validate_on(set: torch.utils.data.Dataset, loader: torch.utils.data.DataLoader, model, in_vocabulary, out_vacabulary) -> Tuple[Any, float, Dict]:
     MODEL_PATH = '/root/projects/transformer_generalization/save/cfq_mcd_small_batch_universal/checkpoint/model-50000.pth'
     LABEL_SMOOTHING = 0.0
 
     model.load_state_dict(torch.load(MODEL_PATH)['model'])
     model.eval()
     model_interface = create_model_interface(model, LABEL_SMOOTHING)
+
+    result = {'results': []}
 
     with torch.no_grad():
         loss_sum = 0
@@ -48,16 +51,38 @@ def validate_on(set: torch.utils.data.Dataset, loader: torch.utils.data.DataLoad
         for d in tqdm(loader):
             d = prepare_data(d)
             res = model_interface(d)
-            digits = model_interface.decode_outputs(res)
+            digits = model_interface.decode_outputs(res)  # outputs, output_lengths
             loss_sum += res.loss.sum().item() * res.batch_size
+
+            predicted_ids_all_batch = torch.argmax(digits[0][:-1, :, : -1], dim=-1)  # TODO: labelよりtokenの方がbatchsize以外の次元が１大きい
+            label_ids_all_batch = d['out']
+            input_ids_all_batch = d['in']
+            # TODO: predictionとlabelはIDが0-96まであるが、out_vacaburary.inv_wordsは0から95まで。
+            # TODO: おそらく、labelの0はpaddingなので、predictionを1-95までにするのが良い気はする。
+            # TODO: 一方で、両方ゼロ始まりの場合最初の数サンプルの予測結果がとても合っているので、predictionだけ１始まりにするように
+            # TODO: ずらすのは何か違う感じがする。
+            for batch_id in range(predicted_ids_all_batch.shape[1]):
+                predicted_tokens = [out_vacabulary.inv_words[int(token_id)] for token_id in predicted_ids_all_batch[:, batch_id]]
+                label_tokens = [out_vacabulary.inv_words[int(label_id)] for label_id in label_ids_all_batch[:, batch_id]]
+                input_tokens = [in_vocabulary.inv_words[int(input_id)] for input_id in input_ids_all_batch[:, batch_id]]
+                result['results'].append(
+                    {
+                        'input_sentence': ' '.join(input_tokens),
+                        'predicted_sentence': ' '.join(predicted_tokens),
+                        'label_sentence': ' '.join(label_tokens),
+                        # 'predicted_tokens': predicted_tokens,
+                        # 'label_tokens': label_tokens,
+                        'exact_match': int(torch.equal(predicted_ids_all_batch[:, batch_id], label_ids_all_batch[:, batch_id]))
+                    }
+                )
 
             test.step(digits, d)
 
-    return test, loss_sum / len(set)
+    return test, loss_sum / len(set), result
 
 
-def create_valid_loader(vset: torch.utils.data.Dataset, test_batch_size, batch_dim, VALID_NUM_WORKERS) -> torch.utils.data.DataLoader:
-    return torch.utils.data.DataLoader(vset, batch_size=test_batch_size,
+def create_valid_loader(vset: torch.utils.data.Dataset, BATCH_SIZE, batch_dim, VALID_NUM_WORKERS) -> torch.utils.data.DataLoader:
+    return torch.utils.data.DataLoader(vset, batch_size=BATCH_SIZE,
                                        collate_fn=framework.loader.collate.VarLengthCollate(batch_dim=batch_dim),
                                        num_workers=VALID_NUM_WORKERS)
 
@@ -91,10 +116,16 @@ def create_model(in_voc_size, out_voc_size) -> torch.nn.Module:
 
 if __name__ == "__main__":
     CFQ_SPLIT = 'mcd2'
-    TEST_BATCH_SIZE = 512
+    BATCH_SIZE = 512
     VALID_NUM_WORKERS = 0
     BATCH_DIM = 1
-    test_set = dataset.CFQ(["test"], split_type=[CFQ_SPLIT])
-    test_loader = create_valid_loader(test_set, TEST_BATCH_SIZE, BATCH_DIM, VALID_NUM_WORKERS)
-    model = create_model(len(test_set.in_vocabulary), len(test_set.out_vocabulary)).to('cuda')
-    validate_on(test_set, test_loader, model)
+    SPLIT = 'val'  # 'train', ’val' or 'test'  TODO: 'train'は厳密に正しいかはわからない。性能を見るに良さそう。
+    SAVE_PATH = f'/root/projects/transformer_generalization/results/cfq_mcd_small_batch_universal_{SPLIT}.json'
+    data_set = dataset.CFQ([SPLIT], split_type=[CFQ_SPLIT])
+    data_loader = create_valid_loader(data_set, BATCH_SIZE, BATCH_DIM, VALID_NUM_WORKERS)
+    model = create_model(len(data_set.in_vocabulary), len(data_set.out_vocabulary)).to('cuda')
+    valid, loss, result = validate_on(data_set, data_loader, model, data_set.in_vocabulary, data_set.out_vocabulary)
+    accuracy = valid.accuracy
+    print(f'loss: {loss}, accuracy: {accuracy}')
+    with open(SAVE_PATH, 'w') as f:
+        json.dump(result, f)
